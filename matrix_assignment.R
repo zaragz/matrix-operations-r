dfb81578cf4e3b94e15bd88bf030793f019be370
zaragz <zaritag115@gmail.com>
2026-02-22 21:54:45 -0500

    Add matrix assignment script for Module 6

---
+++ matrix_assignment.R
@@ -0,0 +1,22 @@
+# Matrix Assignment - Module 6
+# Author: Zara Gonzalez
+
+# Question 1
+A = matrix(c(2,0,1,3), ncol=2)
+B = matrix(c(5,2,4,-1), ncol=2)
+
+# A + B
+A + B
+
+# A - B
+A - B
+
+# Question 2
+diag(c(4,1,2,3))
+
+# Question 3
+M = diag(3,5)
+M[,1] = c(3,2,2,2,2)
+M[1,] = c(3,1,1,1,1)
+
+M
